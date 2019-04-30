0a4208bd00b97bcc5e107903ed3438e50c72b534
Sarkis <sarkis_aghazarian@yahoo.com>
2019-04-29 18:05:39 -0700

    latest

---
+++ src/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+    render(){
+        // Vinicio - render returns JSX
+        return(<h1> Generate Cowsay Lorem </h1>);
+}
+}
